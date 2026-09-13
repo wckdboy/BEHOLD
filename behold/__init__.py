08698a9901d1c3b586c0999dc703f5b42d19802c
wckdboy <boy@wckd.cc>
2026-09-13 22:35:06 +0200

    Bump BEHOLD to 0.4.0 for Blender 5.2 LTS

--- behold/__init__.py
+++ behold/__init__.py
@@ -6,8 +6,8 @@ from __future__ import annotations
 bl_info = {
     "name": "BEHOLD",
     "author": "wckdboy",
-    "version": (0, 3, 0),
-    "blender": (4, 2, 0),
+    "version": (0, 4, 0),
+    "blender": (5, 2, 0),
     "location": "View3D > Sidebar > BEHOLD",
     "description": "Product studio, Light Draw, materials, shoot, and product import",
     "category": "Render",
